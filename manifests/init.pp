0cba3fe8e63eab98c0a422b260a0e61d4dbefcc3
ChrisMcKeownUK <Chris.McKeown@bedegaming.com>
2015-08-13 16:04:06 +0100

    Added script source

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,15 @@
 class newrelic_riak_agent (
-  $license_key    = $newrelic_riak_agent::params::license_key,
-  $port           = $newrelic_riak_agent::params::port,
-  $conf_template  = $newrelic_riak_agent::params::conf_template,
-  $plugin_source  = $newrelic_riak_agent::params::plugin_source,
-  $installdir     = $newrelic_riak_agent::params::installdir,
-  $verbose_output = $newrelic_riak_agent::params::verbose_output,
-  $service_ensure = $newrelic_riak_agent::params::service_ensure,
-  $service_enable = $newrelic_riak_agent::params::service_enable,
-  $initscript     = $newrelic_riak_agent::params::initscript,
-  $initscript_src = $newrelic_riak_agent::params::initscript_src,
+  $license_key     = $newrelic_riak_agent::params::license_key,
+  $port            = $newrelic_riak_agent::params::port,
+  $conf_template   = $newrelic_riak_agent::params::conf_template,
+  $plugin_source   = $newrelic_riak_agent::params::plugin_source,
+  $installdir      = $newrelic_riak_agent::params::installdir,
+  $verbose_output  = $newrelic_riak_agent::params::verbose_output,
+  $service_ensure  = $newrelic_riak_agent::params::service_ensure,
+  $service_enable  = $newrelic_riak_agent::params::service_enable,
+  $initscript      = $newrelic_riak_agent::params::initscript,
+  $initscript_src  = $newrelic_riak_agent::params::initscript_src,
+  $agentscript_src = $newrelic_riak_agent::params::agentscript_src,
 ) inherits newrelic_riak_agent::params {
 
   class { 'newrelic_riak_agent::install': } ->
--- manifests/params.pp
+++ manifests/params.pp
@@ -2,16 +2,17 @@ class newrelic_riak_agent::params {
 
   case $::osfamily {
     'redhat': {
-      $license_key    = undef
-      $port           = 8098
-      $conf_template  = 'newrelic_riak_agent/newrelic_plugin.yml.erb'
-      $plugin_source  = 'newrelic_riak_agent/riak_agent.rb'
-      $installdir     = '/opt/newrelic-riak-agent'
-      $verbose_output = 0
-      $service_ensure = 'running'
-      $service_enable = true
-      $initscript     = '/etc/systemd/system/newrelic-riak-agent.service'
-      $initscript_src = 'puppet:///modules/newrelic_riak_agent/newrelic-riak-agent.service'
+      $license_key     = undef
+      $port            = 8098
+      $conf_template   = 'newrelic_riak_agent/newrelic_plugin.yml.erb'
+      $plugin_source   = 'newrelic_riak_agent/riak_agent.rb'
+      $installdir      = '/opt/newrelic-riak-agent'
+      $verbose_output  = 0
+      $service_ensure  = 'running'
+      $service_enable  = true
+      $initscript      = '/etc/systemd/system/newrelic-riak-agent.service'
+      $initscript_src  = 'puppet:///modules/newrelic_riak_agent/newrelic-riak-agent.service'
+      $agentscript_src = 'puppet:///modules/newrelic_riak_agent/riak-agent.rb'
     }
 
     default: {
